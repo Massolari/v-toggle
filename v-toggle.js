a96e7e87cd2c0a5a0706cbf420689b071ecfb010
Massolari <amrj-01e2123@marinha.mil.br>
2018-01-18 14:31:40 -0200

    First commit

---
+++ v-toggle.js
@@ -0,0 +1,34 @@
+Vue.component('v-toggle', {
+    template:`
+    <div v-on:click="toggleValue" class="toggle btn" :class="toggleClass" style="width: 0px; height: 0px;">
+        <div class="toggle-group">
+            <label class="btn btn-primary toggle-on">{{ onText }}</label>
+            <label class="btn btn-default active toggle-off">{{ offText }}</label>
+            <span class="toggle-handle btn btn-default"></span>
+        </div>
+    </div>`,
+    props: ['on', 'off', 'status', 'onValue', 'offValue'],
+    data() {
+        return {
+            onText: this.on || "Sim",
+            offText: this.off || "Não",
+            currentStatus: (this.status === (this.onValue || true)),
+            onVal: this.onValue || true,
+            offVal: this.offValue || false
+        }
+    },
+    methods: {
+        toggleValue() {
+            this.currentStatus = !this.currentStatus
+            this.$emit('change', (this.currentStatus) ? this.onVal : this.offVal)
+        }
+    },
+    computed: {
+        toggleClass() {
+            return {
+                'btn-primary': this.currentStatus,
+                'btn-default off': !this.currentStatus
+            }
+        }
+    }
+})
