c7d395f87926b726b136eeee054401540dac5843
Massolari <amrj-01e2123@marinha.mil.br>
2018-01-30 11:21:21 -0200

    Now the component accepts a disabled prop

--- v-toggle.js
+++ v-toggle.js
@@ -1,13 +1,13 @@
 Vue.component('v-toggle', {
     template:`
-    <div v-on:click="toggleValue" class="toggle btn" :class="toggleClass" style="width: 0px; height: 0px;">
+    <div v-on:click="toggleValue" class="toggle btn" :class="toggleClass" style="width: 0px; height: 0px;" :disabled="disabled">
         <div class="toggle-group">
-            <label class="btn btn-primary toggle-on">{{ onText }}</label>
-            <label class="btn btn-default active toggle-off">{{ offText }}</label>
-            <span class="toggle-handle btn btn-default"></span>
+            <label class="btn btn-primary toggle-on" :style="styleCursor">{{ onText }}</label>
+            <label class="btn btn-default active toggle-off" :style="styleCursor">{{ offText }}</label>
+            <span class="toggle-handle btn btn-default" :style="styleCursor"></span>
         </div>
     </div>`,
-    props: ['on', 'off', 'status', 'onValue', 'offValue'],
+    props: ["on", "off", "status", "onValue", "offValue", "disabled"],
     data() {
         return {
             onText: this.on || "Sim",
@@ -18,6 +18,9 @@ Vue.component('v-toggle', {
     },
     methods: {
         toggleValue() {
+            if (this.disabled) {
+                return
+            }
             this.$emit('change', (!this.currentStatus) ? this.onVal : this.offVal)
         }
     },
@@ -26,6 +29,11 @@ Vue.component('v-toggle', {
             return {
                 'btn-primary': this.currentStatus,
                 'btn-default off': !this.currentStatus
+            }
+        },
+        styleCursor() {
+            return {
+                cursor: (this.disabled) ? "not-allowed" : "pointer"
             }
         },
         currentStatus() {
